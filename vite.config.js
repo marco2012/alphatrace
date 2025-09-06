13e4dbe76f7896e9e9b21341d44106acf6cb10a1
marco2012 <marco.fox.marco@gmail.com>
2025-09-06 12:25:59 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/AlphaTrace/",
+    base: "/alphatrace/",
+    build: {
+        outDir: "dist",
+        assetsDir: "assets",
+    },
 });
